a574b620ff529a0c39c16846503616384ead1d96
Embret Olav Rasmussen Roås <eoroaas@stud.ntnu.no>
2024-02-28 17:23:10 +0100

    div

--- prisma/seed.sh
+++ prisma/seed.sh
@@ -9,7 +9,7 @@ DATABASE_URL="postgresql://usr:pw@localhost:5433/db"
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Use psql to run the SQL file using the connection URL
-psql "$DATABASE_URL" -f "$SCRIPT_DIR/scripts/create_tables.sql"
-psql "$DATABASE_URL" -f "$SCRIPT_DIR/scripts/fakturaoversikt.sql"
-psql "$DATABASE_URL" -f "$SCRIPT_DIR/scripts/saldobalanse.sql"
-psql "$DATABASE_URL" -f "$SCRIPT_DIR/scripts/timestatistikk.sql"
+psql -f "$SCRIPT_DIR/scripts/create_tables.sql" "$DATABASE_URL" 
+psql -f "$SCRIPT_DIR/scripts/fakturaoversikt.sql" "$DATABASE_URL" 
+psql -f "$SCRIPT_DIR/scripts/saldobalanse.sql" "$DATABASE_URL" 
+psql -f "$SCRIPT_DIR/scripts/timestatistikk.sql" "$DATABASE_URL" 
--- src/app/fakturaoversikt/page.tsx
+++ src/app/fakturaoversikt/page.tsx
@@ -1,8 +1,8 @@
 import { PlusIcon } from "@heroicons/react/24/outline";
+import { api } from "../../trpc/server";
 import { Button } from "../_components/button";
 import Header from "../_components/header";
 import Invoices from "../_components/invoices";
-import { api } from "../../trpc/server";
 
 export default async function Home() {
   const invoices = await api.faktura.getAll.query({ skip: 0, take: 10 });
@@ -14,7 +14,7 @@ export default async function Home() {
         breadcrumb={["Hjem", "Fakturaoversikt"]}
         primaryCta={
           <Button>
-            Ny faktura <PlusIcon className="h-4 w-4" />
+            Ny faktura <PlusIcon className="ml-2 h-4 w-4" />
           </Button>
         }
       />
