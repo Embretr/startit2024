88a0f046474331ec07f0128ee85fabd0c7ea5fca
Embret Olav Rasmussen Roås <eoroaas@stud.ntnu.no>
2024-02-28 20:24:46 +0100

    aa

--- src/app/fakturaoversikt/ny/page.tsx
+++ src/app/fakturaoversikt/ny/page.tsx
@@ -121,7 +121,7 @@ export default function Page() {
               }}
               className="block w-full rounded-md bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              {loading ? "laster" : "Send faktura"}
+              {loading ? "laster..." : "Send faktura"}
             </button>
           </div>
         </form>
